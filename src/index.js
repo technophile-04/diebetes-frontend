62497c70b9737d16dd3eebdf9718cf3708df4cd2
Shiv Bhonde <shivbhonde04@gmail.com>
2022-12-02 23:59:11 +0530

    setup basic chakra and wagmi repo

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { WagmiConfig, createClient, configureChains, chain } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.optimism],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <WagmiConfig client={client}>
+      <ColorModeScript />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </WagmiConfig>
   </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

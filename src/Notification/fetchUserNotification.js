1906e8b453a39c8da57f0760d18a2b009fdcd696
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-04 03:37:11 +0530

    Update Push Notification

--- src/Notification/fetchUserNotification.js
+++ src/Notification/fetchUserNotification.js
@@ -1,3 +1,10 @@
+import * as PushAPI from "@pushprotocol/restapi";
+import * as ethers from "ethers";
+
+const PK = process.env.CHANNEL_PRIVATE_KEY;
+const Pkey = `0x${PK}`;
+const signer = new ethers.Wallet(Pkey);
+
 export const fetchUserNotification = async (address) => {
     const notifications = await PushAPI.user.getFeeds({
         user: `eip155:5:${address}`, // user address in CAIP

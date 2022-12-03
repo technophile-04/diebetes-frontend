8cae9a2c05113d36e3a3a59c03c13132398098f2
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-03 14:36:10 +0530

    Added the Push Chat

--- src/pages/Proposal.jsx
+++ src/pages/Proposal.jsx
@@ -16,8 +16,8 @@ import {
   List,
   ListItem,
 } from '@chakra-ui/react';
-import { MdLocalShipping } from 'react-icons/md';
 import ProposalImage from "../img/proposal1.jpg";
+import { Chat } from "@pushprotocol/uiweb";
 
 const Proposal = () => {
   return (
@@ -109,6 +109,12 @@ const Proposal = () => {
           </Stack>
         </SimpleGrid>
       </Container>
+      <Chat
+        account="0x6430C47973FA053fc8F055e7935EC6C2271D5174" //user address
+        supportAddress="0xd9c1CCAcD4B8a745e191b62BA3fcaD87229CB26d" //support address
+        apiKey="jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0"
+        env="staging"
+      />
       <WorldcoinIntegrationButton />
     </div>
   );
